43d05021d3a8783f46c58294b8b7ec6ef1b063de
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-20 11:44:06 -0800

    javaScript function that generates a random pattern of red,blue,green,yellow

---
+++ simon.js
@@ -0,0 +1,22 @@
+let pattern = [];
+let htmlButtonNames = ['blue','green', 'red', 'yellow'];
+
+function randomPattern() {
+  let number = Math.floor(Math.random() * 4);
+
+  pattern.push(htmlButtonNames[number]);
+
+  //THIS IS FOR LATER SO THE GAME GOES ON UNTIL THE USER LOSES.
+  // if (userInput === pattern){
+  //   randomPattern();
+
+  // } else if ( userInput !== pattern){
+  //   console.log(`Game Over! Better luck next time! You reached level ${pattern.length}!`);
+  // }
+}
+
+// SANITY CHECKS FOR STHE randomPattern FUNCTION
+// for (let a=1; a<=5; a++){
+// randomPattern();
+// }
+// console.log(pattern);
