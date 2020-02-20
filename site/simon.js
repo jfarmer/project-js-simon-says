e7f296b0aaf88d10c16fcaeb3e21a17139850b2c
Patrick Sullivan <pasullivan@davidson.edu>
2020-02-20 14:17:20 -0800

    simon1

--- site/simon.js
+++ site/simon.js
@@ -1,2 +1,46 @@
-// This file contains the game logic.
-// All the event-listening should happen in buttons.js
+let buttons = console.require("./buttons");
+let begin = false;
+
+function start() {
+begin = true;
+let highlightColors = ["green","red","yellow","blue"];
+let currentColor = "";
+let userColor;
+}
+
+let moves = [1,1,3];
+
+function greenHighlight() {
+  $('.simon-button.green').*transform*
+}
+
+
+function userClick() {
+  $('.simon-button').on('click', function() {
+      if *click* on *color*
+      {
+        userColor = color;
+      }
+  }
+}
+
+function simon() {
+
+}
+
+function startingMove(begin) {
+  if (begin === true) {
+    greenHighlight();
+    currentColor = highlightColors[0];
+    if (currentColor === "green")
+    {
+      userClick();
+      if (userColor = currentColor) {
+        simon();
+      }
+      else {
+        *GAME OVER*
+      }
+    }
+  }
+}
