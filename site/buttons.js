1a0022d23d34e8ec73e19254793e378f39c6668c
Isaac Snow <isaacjsnow@gmail.com>
2020-07-08 10:55:25 -0700

    lint

--- site/buttons.js
+++ site/buttons.js
@@ -1,19 +1,19 @@
-const greenButton = document.querySelector(".simon-button.green");
-greenButton.addEventListener('click', function () {
+const greenButton = document.querySelector('.simon-button.green');
+greenButton.addEventListener('click', function() {
   alert('You clicked the green button');
 });
 
-const redButton = document.querySelector(".simon-button.red");
-redButton.addEventListener('click', function () {
+const redButton = document.querySelector('.simon-button.red');
+redButton.addEventListener('click', function() {
   alert('You clicked the red button');
 });
 
-const yellowButton = document.querySelector(".simon-button.yellow");
-yellowButton.addEventListener('click', function () {
+const yellowButton = document.querySelector('.simon-button.yellow');
+yellowButton.addEventListener('click', function() {
   alert('You clicked the yellow button');
 });
 
-const blueButton = document.querySelector(".simon-button.blue");
-blueButton.addEventListener('click', function () {
+const blueButton = document.querySelector('.simon-button.blue');
+blueButton.addEventListener('click', function() {
   alert('You clicked the blue button');
 });
