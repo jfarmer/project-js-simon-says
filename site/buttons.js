833fbe4169c9b17d5abb65f9e457c940ef23cf38
Isaac Snow <isaacjsnow@gmail.com>
2020-07-08 10:54:16 -0700

    assign variables closer to usage

--- site/buttons.js
+++ site/buttons.js
@@ -1,20 +1,19 @@
 const greenButton = document.querySelector(".simon-button.green");
-const redButton = document.querySelector(".simon-button.red");
-const yellowButton = document.querySelector(".simon-button.yellow");
-const blueButton = document.querySelector(".simon-button.blue");
-
 greenButton.addEventListener('click', function () {
   alert('You clicked the green button');
 });
 
+const redButton = document.querySelector(".simon-button.red");
 redButton.addEventListener('click', function () {
   alert('You clicked the red button');
 });
 
+const yellowButton = document.querySelector(".simon-button.yellow");
 yellowButton.addEventListener('click', function () {
   alert('You clicked the yellow button');
 });
 
+const blueButton = document.querySelector(".simon-button.blue");
 blueButton.addEventListener('click', function () {
   alert('You clicked the blue button');
 });
